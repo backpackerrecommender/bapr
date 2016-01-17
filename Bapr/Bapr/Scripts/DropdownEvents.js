1a22b22d82cd5d8c6943ec22865518cacd0a4955
OanaAghebanoae <oana.aghebanoae@gmail.com>
2016-01-17 11:34:56 +0200

    Added autocomplete for new locations.

--- Bapr/Bapr/Scripts/DropdownEvents.js
+++ Bapr/Bapr/Scripts/DropdownEvents.js
@@ -1,10 +1,34 @@
 ﻿function GetLocation() {
     if (navigator.geolocation) {
         var pos = navigator.geolocation.getCurrentPosition(function (position) {
-            alert(position.coords.latitude + " " + position.coords.longitude);
+            var button = document.getElementById("useCurrentLocation");
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude
+            button.setAttribute("data-lat", lat);
+            button.setAttribute("data-lng", lng);
+            alert(lat + " " + lng);
         });
     } else {
         alert("not supported");
-        // x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
+
+function initialize() {
+    var input = document.getElementById("userOtherLocation");
+    var options = {
+        language: 'en-GB',
+        types: ['(cities)']
+    }
+    var autocomplete = new google.maps.places.Autocomplete(input, options);   
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        input.value = place.name;
+        input.setAttribute("data-lat", lat);
+        input.setAttribute("data-lng", lng);
+        alert(lat + " " + lng);
+    });
+   
+}
+
