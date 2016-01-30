cc3c2b2b999497ee1ba44c235852b958f34434f5
Simona Andries <simoandries@gmail.com>
2016-01-30 14:06:34 +0200

    again

---
+++ Bapr/BaprAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using BrightstarDB.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaprAPI.Models
+{
+    public interface IUser
+    {
+        string Email { get; set; }
+
+        string Password { get; set; }
+
+        IUserPreference UserPreference {get; set;}
+    }
+}
---
+++ Bapr/BaprAPI/Models/UserPreference.cs
@@ -0,0 +1,25 @@
+﻿using BrightstarDB.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaprAPI.Models
+{
+    [Entity]
+    public interface IInterest
+    {
+        string Name { get; set; }
+        bool Checked { get; set; }
+    }
+
+    [Entity]
+    public interface IUserPreference
+    {
+        ICollection<IInterest> Interests { get; set; }
+        ICollection<IInterest> Cuisine { get; set; }
+        long MaxBudget { get; set; }
+        int NoOfDays { get; set; }
+        bool NeedMedicalSupport { get; set; }
+    }
+}
